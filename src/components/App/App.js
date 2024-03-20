bdcf5ca33f98a4d44ee146adfd55a8b0979ae6ad
Gauthier <144640575+GauthierBCC@users.noreply.github.com>
2024-03-20 15:05:00 +1000

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Currency converter
+      </header>
+    </div>
+  );
+}
+
+export default App;
